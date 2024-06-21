32d422c05aad6bf0e3476138a58fba3d93af982c
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-06-21 08:34:27 +0530

    changes

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def root():
-    return{"message": "Hello World !"}
+    return{"message": "Hello World."}
+
+
+@app.get("/posts")
+def get_posts():
+    return{"Data": "This is your posts."}
