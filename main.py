5b9540424e5989d3179a5446204d695c0fcbbf40
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-06-30 14:16:07 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import Body, FastAPI
+from typing import Optional
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -6,20 +7,21 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 
 @app.get("/")
 def root():
     return{"message": "Hello World."}
 
-
 @app.get("/posts")
 def get_posts():
     return{"Data": "This is your posts."}
 
-
 @app.post("/createposts")
 def create_posts(new_post: Post):
     print(new_post)
+    print(new_post.model_dump())
     return{"data": "data received"}
 
