fc5e66b16d23a8f0f9fc83c9b58206f9e56a74f6
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-07-04 23:43:18 +0530

    changes

--- main.py
+++ main.py
@@ -17,11 +17,13 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return{"Data": "This is your posts."}
+    return{"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/posts")
 def create_posts(post: Post):
     print(post)
     print(post.model_dump())
     return{"data": "data received"}
 
+
+my_posts = [{"title": "title of post 1", "content": "Content of post 1", "id": 1}, {"title": "title of post 2", "content": "Content of post 2", "id": 2}]
