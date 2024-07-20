082fea93585f6da50e2a5510024ff9da3996aef6
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-07-20 23:38:46 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -6,19 +6,14 @@ import psycopg
 import time
 # from psycopg.rows import dict_row
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, get_db
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+
 
 class Post(BaseModel):
     title: str
