1f486d954a9b683be0e07ba34d24c8278d9f7c12
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-07-21 20:33:40 +0530

    chore: Refactor code to use SQLAlchemy for database operations

--- app/main.py
+++ app/main.py
@@ -43,66 +43,82 @@ def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return {"status": "success"}
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""
-        SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
+def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #     SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    # print(posts)
+    posts = db.query(models.Post).all()
     return{"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    cursor.execute("""
-        INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
-        (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #     INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""", 
+    #     (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
     # print(post)
     # print(post.model_dump())
     # post_dict = post.model_dump()
     # post_dict['id'] = randrange(0, 1000000)
     # my_posts.append(post_dict)
+
+    # new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return{"data": new_post}
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    cursor.execute("""
-        SELECT * FROM posts WHERE id = %s """, ((str(id)), ))
-    test_post = cursor.fetchone()
-    print(test_post)
+def get_post(id: int, response: Response, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #     SELECT * FROM posts WHERE id = %s """, ((str(id)), ))
+    # test_post = cursor.fetchone()
+    # print(test_post)
     # post = find_post(id)
-    if not test_post:
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    
+    if not post:
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return{"data": f"post with id: {id} was not found"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
-    return{"data": test_post}
+    return{"data": post}
 
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
+# def find_post(id):
+#     for p in my_posts:
+#         if p['id'] == id:
+#             return p
         
 
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
+# def find_index_post(id):
+#     for i, p in enumerate(my_posts):
+#         if p['id'] == id:
+#             return i
         
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    cursor.execute("""
-                   DELETE FROM posts WHERE id = %s RETURNING *""", ((str(id)), ))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+def delete_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #                DELETE FROM posts WHERE id = %s RETURNING *""", ((str(id)), ))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+
+    
+
+
     if deleted_post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
 
