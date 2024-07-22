7ac7cee9c63212805ef3f43d906301aec2150df0
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-07-22 23:44:38 +0530

    chore: Refactor update_post function to use SQLAlchemy for database operations

--- app/main.py
+++ app/main.py
@@ -129,19 +129,24 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    cursor.execute("""
-                   UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
-                   (post.title, post.content, post.published, (str(id))))
-    updated_post = cursor.fetchone()
-    conn.commit()
+def update_post(id: int, updated_post: Post, db: Session = Depends(get_db)):
+    # cursor.execute("""
+    #                UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
+    #                (post.title, post.content, post.published, (str(id))))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
     
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
 
-    if updated_post is None:
+    if post is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     
     # post_dict = post.model_dump()
     # post_dict["id"] = id
     # my_posts[index] = post_dict
 
-    return{"data": updated_post}
+    post_query.update(updated_post.model_dump(), synchronize_session=False)
+    db.commit()
+
+    return{"data": post_query.first()}
