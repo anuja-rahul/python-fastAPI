fcc26c8cd0f6b8471275ea7e22bbccaac4a4c92d
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-08-02 23:40:35 +0530

    verifying access token

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,12 @@
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timedelta
+from . import schemas
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 load_dotenv()
 
@@ -24,4 +28,25 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+
+def verify_access_token(token: str, credentials_exception):
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id: str = payload.get("user_id")
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWTError:
+        raise credentials_exception
+    
+    return token_data
+    
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                          detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    return verify_access_token(token, credentials_exception)    
     
