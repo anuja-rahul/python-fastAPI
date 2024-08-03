c462bb2bf32375f751e6b59b5affac40724433e9
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-08-03 09:12:29 +0530

    chore: Refactor routers to include user authentication in get_posts, delete_post, and update_post

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,7 +11,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""
     #     SELECT * FROM posts""")
     # posts = cursor.fetchall()
@@ -72,7 +72,7 @@ def get_post(id: int, response: Response, db: Session = Depends(get_db), user_id
         
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""
     #                DELETE FROM posts WHERE id = %s RETURNING *""", ((str(id)), ))
     # deleted_post = cursor.fetchone()
@@ -91,7 +91,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), 
+                user_id: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""
     #                UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", 
     #                (post.title, post.content, post.published, (str(id))))
