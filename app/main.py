4834f2c53f4332629cc6c809ab592b48bf9e9d1c
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-08-06 23:08:36 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -8,14 +8,14 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
+
 @app.get("/")
 def root():
-    return{"message": "Hello World."}
+    return{"message": "Welcome to python-fastAPI."}
 
 
 # @app.get("/sqlalchemy")
